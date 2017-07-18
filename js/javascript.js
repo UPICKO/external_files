bd5b3feee67fc12ebec9b69cf408e4261269c5a3
franktao1 <franktao1@nbnco.com.au>
2017-07-18 11:16:50 +1000

    N/A

--- js/javascript.js
+++ js/javascript.js
@@ -68,8 +68,10 @@ $(function() {
             alert("Your browser is not supported. Please consider using Chrome, Firefox or Microsoft Edge");
         } else {
             $('#homepage-filters').remove();
+            $("body").append("<div id='spinner_landing_page' style='text-align: center'><img width='80em' src='https://github.com/UPICKO/external_files/raw/master/images/spinner.gif'/></div>");
             $.getScript( "https://drive.google.com/uc?export=download&id=0B5fEP3aSxchLVzZBLTZBZUlWZ0E", function( data, textStatus, jqxhr ) {
                 $("body").append(landingPageHtml);
+                $("#spinner_landing_page").remove();
             });
         }
     } else if(window.location.href == 'https://www.upicko.com/en/' || window.location.href == 'https://www.upicko.com/en') {
